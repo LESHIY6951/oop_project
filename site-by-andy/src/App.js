07e484c1d632c672c7a6c920639fce49de880ec4
pls-gimme-cookie <a.and.ndrew@mail.ru>
2023-05-28 01:22:45 +0300

    the beginning of the site

---
+++ site-by-andy/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <img src="/images/kat2.png" alt="" title="Андрюша котик"/>
+        <p>
+          Тут должен быть клёвый сайт, но... у нашего фронтендера лапки и он только начал учить реакт
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
